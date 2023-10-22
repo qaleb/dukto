7202a5ebabc55d117ad970396cc72c2f50f27298
qaleb <drmaangi@gmail.com>
2023-10-22 22:11:33 +0300

    Resolve errors when building on windows by the commenting out the errors. Will resolve the bugs later.

--- src/platform.cpp
+++ src/platform.cpp
@@ -34,13 +34,13 @@
 #include <windows.h>
 #include <lmaccess.h>
 
-typedef struct _USER_INFO_24 {
-  BOOL   usri24_internet_identity;
-  DWORD  usri24_flags;
-  LPWSTR usri24_internet_provider_name;
-  LPWSTR usri24_internet_principal_name;
-  PSID   usri24_user_sid;
-} USER_INFO_24, *PUSER_INFO_24, *LPUSER_INFO_24;
+//typedef struct _USER_INFO_24 {
+//  BOOL   usri24_internet_identity;
+//  DWORD  usri24_flags;
+//  LPWSTR usri24_internet_provider_name;
+//  LPWSTR usri24_internet_principal_name;
+//  PSID   usri24_user_sid;
+//} USER_INFO_24, *PUSER_INFO_24, *LPUSER_INFO_24;
 
 
 #endif
